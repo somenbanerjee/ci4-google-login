6159f2fcde5e6b393c0401678fac89faeb531211
Somen Banerjee <somen.banerjee0@gmail.com>
2023-09-19 23:30:05 +0530

    update UI and add dashboard UI

---
+++ app/Views/dashboard.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Sign Up</title>
+    <!-- Font Awesome -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet" />
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" />
+    <!-- MDB -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.1/mdb.min.css" rel="stylesheet" />
+</head>
+
+<body>
+    <section class="vh-100" style="background-color: #eee;">
+        <div class="container py-5 h-100">
+            <div class="row d-flex justify-content-center align-items-center h-100">
+                <div class="col-md-12 col-xl-4">
+
+                    <div class="card" style="border-radius: 15px;">
+                        <div class="card-body text-center">
+                            <div class="mt-3 mb-4">
+                                <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava2-bg.webp" class="rounded-circle img-fluid" style="width: 100px;" />
+                            </div>
+                            <h4 class="mb-2">Somen Banerjee</h4>
+                            <p class="text-muted mb-4">somen@Programmer.com</p>
+                            <div class="mb-4 pb-2">
+                                <button type="button" class="btn btn-outline-primary btn-floating">
+                                    <i class="fab fa-facebook-f fa-lg"></i>
+                                </button>
+                                <button type="button" class="btn btn-outline-primary btn-floating">
+                                    <i class="fab fa-twitter fa-lg"></i>
+                                </button>
+                                <button type="button" class="btn btn-outline-primary btn-floating">
+                                    <i class="fab fa-skype fa-lg"></i>
+                                </button>
+                            </div>
+                            <button type="button" class="btn btn-danger btn-rounded btn-lg">
+                                Logout
+                            </button>
+
+                            <p class="pt-4 mb-0">By Somen Banerjee</p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- MDB -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.1/mdb.min.js"></script>
+</body>
+
+</html>
--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -6,65 +6,48 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" />
-    <title>Sign Up</title>
+    <!-- Font Awesome -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet" />
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" />
+    <!-- MDB -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.1/mdb.min.css" rel="stylesheet" />
 </head>
 
 <body>
-    <div class="container">
-        <div class="row pt-4">
-            <div class="col-md-6 offset-md-3">
-                <h3 class="text-center pb-4">CI4 Google Login</h3>
+    <section class="vh-100">
+        <div class="container py-5 h-100">
+            <div class="row d-flex justify-content-center align-items-center h-100">
+                <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+                    <div class="card shadow-2-light">
+                        <div class="card-body p-5 text-center">
 
-                <form action="#">
-                    <div class="row shadow-lg p-5 pb-3">
-                        <fieldset class="form-group col-lg-12">
-                            <div class="form-floating mb-3">
-                                <input type="email" class="form-control" id="email" name="email" placeholder="name@example.com" autofocus />
-                                <label for="email">Email</label>
+                            <h3 class="mb-5">CI4 Google Sign in</h3>
+
+                            <div class="form-outline mb-4">
+                                <input type="email" id="typeEmailX-2" class="form-control form-control-lg" />
+                                <label class="form-label" for="typeEmailX-2">Email</label>
                             </div>
-                        </fieldset>
 
-                        <fieldset class="form-group col-lg-12">
-                            <div class="form-floating mb-3">
-                                <input type="password" class="form-control" id="password" name="password" placeholder="Password" />
-                                <label for="password">Password</label>
+                            <div class="form-outline mb-4">
+                                <input type="password" id="typePasswordX-2" class="form-control form-control-lg" />
+                                <label class="form-label" for="typePasswordX-2">Password</label>
                             </div>
-                        </fieldset>
 
-                        <div class="d-grid">
-                            <button class="btn btn-primary btn-lg" type="submit">
-                                Login
-                            </button>
-                        </div>
+                            <button class="btn btn-primary btn-lg btn-block" type="submit">Login</button>
+                            <div class="my-2">OR</div>
+                            <button class="btn btn-lg btn-block btn-primary" style="background-color: #dd4b39;" type="submit"><i class="fab fa-google me-2"></i> Sign in with google</button>
 
-                        <div class="form-group col-lg-12 mx-auto d-flex align-items-center my-4">
-                            <div class="border-bottom w-100 ml-5"></div>
-                            <span class="px-2 small text-muted font-weight-bold text-muted">OR</span>
-                            <div class="border-bottom w-100 mr-5"></div>
-                        </div>
-
-                        <div class="d-grid mb-4">
-                            <a href="#" class="btn btn-danger btn-lg" asp-page-handler="Register" type="submit">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-google" viewBox="0 0 16 16">
-                                    <path d="M15.545 6.558a9.42 9.42 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.689 7.689 0 0 1 5.352 2.082l-2.284 2.284A4.347 4.347 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.792 4.792 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.702 3.702 0 0 0 1.599-2.431H8v-3.08h7.545z"></path>
-                                </svg>
-                                Sign in with Google
-                            </a>
-                        </div>
-
-                        <!-- Not Registered -->
-                        <div class="text-center w-100 mt-4">
-                            <p class="text-muted font-weight-bold">
-                                By Somen Banerjee
-                            </p>
+                            <p class="pt-4 mb-0">By Somen Banerjee</p>
                         </div>
                     </div>
-                </form>
+                </div>
             </div>
         </div>
-    </div>
+    </section>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- MDB -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.1/mdb.min.js"></script>
 </body>
 
 </html>
