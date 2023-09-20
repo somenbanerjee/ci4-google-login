7e020bc538b5fe34356b5d58870313c4b59af4c6
Somen Banerjee <somen.banerjee0@gmail.com>
2023-09-20 23:49:16 +0530

    stuck with php autoload

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,13 +2,21 @@
 
 namespace App\Controllers;
 
+use Google\Service\ChromeManagement\GoogleTypeDate;
+
 class Home extends BaseController
 {
     public function index(): string
     {
         return view('login');
     }
-
+    public function login(): string
+    {
+        $client = new GoogleTypeDate;
+        $client->setApplicationName("Client_Library_Examples");
+        $client->setDeveloperKey("YOUR_APP_KEY");
+        return view('login');
+    }
     public function dashboard(): string
     {
         return view('dashboard');
